cf9d6da26e3035d5d010d5a6ba573d5eb68f866a
Sanakey <ask@localhost.localdomain>
2019-03-24 15:50:44 +0000

    created linux branch

---
+++ dhcpbinding.py
@@ -0,0 +1,8 @@
+from kamene.all import *
+import os
+import sys
+import elevate
+
+# request for root/ administrative rights
+elevate.elevate()
+
---
+++ sai.py
@@ -0,0 +1,15 @@
+import arpcache
+from  kamene.all import *
+
+class saiCache(arpcache):
+
+    def __init__(self):
+        pass
+
+
+    def validateReply(self):
+        """
+        Validate all forms of arp reply messages
+        :return: 
+        """""
+        pass
